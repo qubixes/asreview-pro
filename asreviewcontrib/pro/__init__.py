7396595bad3e0e725fd4bfc60a1071c7f9c8c4fc
Raoul Schram <r.d.schram@uu.nl>
2020-05-14 16:18:59 +0200

    Bump version

--- asreviewcontrib/pro/__init__.py
+++ asreviewcontrib/pro/__init__.py
@@ -3,5 +3,5 @@ from asreviewcontrib.pro.bm25 import BM25
 from asreviewcontrib.pro.entrypoint import OracleEntryPoint
 from asreviewcontrib.pro.scibert import Scibert
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __extension_name__ = "asreview-pro"
